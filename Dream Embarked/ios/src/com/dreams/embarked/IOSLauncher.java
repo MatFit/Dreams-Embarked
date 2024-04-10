840e704d96a5db7fabbcba3b88887f60b69b215c
Mathis Fituwi <mafi7060@colorado.edu>
2024-04-10 11:52:33 -0600

    First commit

---
+++ Dream Embarked/ios/src/com/dreams/embarked/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.dreams.embarked;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.dreams.embarked.Platform;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Platform(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
