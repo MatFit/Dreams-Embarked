b2d393ed0ce52cd2d22f8392122110c905095cfc
Mathis Fituwi <mafi7060@colorado.edu>
2024-04-12 08:45:21 -0600

    camera

--- Dream Embarked/core/src/com/dreams/embarked/Platform.java
+++ Dream Embarked/core/src/com/dreams/embarked/Platform.java
@@ -2,6 +2,7 @@ package com.dreams.embarked;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -10,18 +11,26 @@ public class Platform extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Player player;
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("Color-blue.jpg");
 		player = Player.getInstance(1,"name", img);
+		camera = new OrthographicCamera();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 
+		float cameraX = (Gdx.graphics.getWidth() - player.grabTexture().getWidth() / 2);
+		float cameraY = (Gdx.graphics.getHeight() - player.grabTexture().getHeight() / 2);
+		camera.position.set(cameraX, cameraY, 0);
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(player.grabTexture(), player.grabPlayerX(), player.grabPlayerY());
 		batch.end();
