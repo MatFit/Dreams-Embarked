de0d94369b11de582c2fbd630c577efb6d20dbdc
Reda Razza <94404873+rera8642@users.noreply.github.com>
2024-04-12 20:33:08 -0600

    Box setup

---
+++ Dream Embarked/core/src/com/dreams/embarked/CollisionBox.java
@@ -0,0 +1,23 @@
+package com.dreams.embarked;
+
+public class CollisionBox {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private String tag;
+    private Renderable owner;
+
+    public CollisionBox(int x, int y, int width, int height, String tag, Renderable owner) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.tag = tag;
+        this.owner = owner;
+    }
+
+    public void Trigger() {
+
+    }
+}
